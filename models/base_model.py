257cd82974aa6fea343ea9f266e2f95466ec1e3f
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-27 23:44:35 +0100

    base_model: create an instance of sqlalchemy named  Base

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,26 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
+from sqlalchemy import Column, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    id = Column(String(60), primary_key=True)
+    create_at = Column(Date, nullable=False, default=datetime.utcnow())
+    update_at = Column(Date, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -29,8 +37,8 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +49,12 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        try:
+            del dictionary['_sa_instance_state']
+        except KeyError:
+            pass
         return dictionary
+
+    def delete(self):
+        """Delete current instance from storage"""
+        storage.delete(self)
