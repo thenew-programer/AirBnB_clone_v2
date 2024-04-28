5e90c921d200e0e1a313eac9b2d1b8599e6f01a1
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-28 12:00:20 +0100

    city: update city class to represent attributes for db storage type aswell as file type

--- models/city.py
+++ models/city.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column, ForeignKey
 
@@ -7,5 +9,10 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if os.getenv('HBNB_STORAGE_TYPE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    else:
+        name = ''
+        state_id = ''
+
