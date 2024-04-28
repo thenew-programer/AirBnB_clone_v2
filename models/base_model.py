db6bf705db5a39cc0b70f0c2cf8357d760942da3
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-28 14:00:15 +0100

    correct keyError in base_model when instantianting with **kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -24,11 +24,14 @@ class BaseModel:
             self.updated_at = datetime.now()
             models.storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            try:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
+            except KeyError:
+                pass
             self.__dict__.update(kwargs)
 
     def __str__(self):
