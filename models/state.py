f09e716eba70fb41a19fc11af405268fe10259d8
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-28 12:00:56 +0100

    state: update State class to represent attributes for db storage and file storage type,

--- models/state.py
+++ models/state.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 import models
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_STORAGE_TYPE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', passive_deletes=True, backref='state')
+    else:
+        name = ''
 
+        @property
+        def cities(self):
+            """
+            cities getter to retreive cities
+            related to the current state
+            """
+            cities = []
+            for value in models.storage.all().values():
+                try:
+                    if value.state_id == self.id:
+                        cities.append(value)
+                except AttributeError:
+                    pass
+            return cities
