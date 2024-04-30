a7998d9104d593e4e03bc2420e447a3c6a51c0bc
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-30 17:20:10 +0100

    baseModel

--- models/base_model.py
+++ models/base_model.py
@@ -23,19 +23,19 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-	if kwargs:
-	    for key, value in kwargs.items():
-		if key == 'created_at' or key == 'updated_at':
-		    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-		    if key != '__class__' and hasattr(self.__class__, key):
-			setattr(self, key, value)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__' and hasattr(self.__class__, key):
+                    setattr(self, key, value)
+            else:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
+                self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
