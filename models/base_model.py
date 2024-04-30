b0efeed2731373dc1c5ac14e9ebbef9831f563f8
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-30 02:11:09 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -29,24 +29,6 @@ class BaseModel:
             if 'id' not in kwargs:
                 setattr(self, 'id', str(uuid.uuid4()))
 
-        '''
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(
-                    kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(
-                    kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
-                self.id = str(uuid.uuid4())
-        '''
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
