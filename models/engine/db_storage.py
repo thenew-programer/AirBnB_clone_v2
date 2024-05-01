119954b45c1451b132298007563b21637ca14535
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-05-02 00:30:57 +0100

    console problem

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,14 +9,7 @@ from sqlalchemy.orm import (
     relationship
 )
 from models.base_model import Base
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 from models import classes
-import models
 
 
 class DBStorage:
