fcccfe889d02be9c3525db1d563e11ba91adccfd
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-05-02 00:11:27 +0100

    rebase conflict

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy import String, Column
-from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
 class User(BaseModel, Base):
@@ -11,3 +12,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship('Place', backref='user', cascade='delete')
